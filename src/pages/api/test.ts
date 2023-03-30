af2e8314743a0faf271061ecb1cd994f96feb6fa
liuw <liuw5367@gmail.com>
2023-03-30 18:24:16 +0800

    update

--- src/pages/api/test.ts
+++ src/pages/api/test.ts
@@ -1,10 +1,8 @@
 import type { APIRoute } from "astro";
-import { getEnv } from "./_utils";
 
 
 export const post: APIRoute = async (context) => {
   const body = await context.request.json();
-  const env = getEnv();
 
   console.log("server request: ", body);
   console.log("server key: ", [
@@ -23,7 +21,6 @@ export const post: APIRoute = async (context) => {
         [import.meta.env.PUBLIC_APP_KEY, import.meta.env.APP_KEY],
         [import.meta.env.PUBLIC_APP_SECRET, import.meta.env.APP_SECRET],
       ],
-      env
     })
   );
 };
